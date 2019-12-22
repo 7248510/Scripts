ea4b0af6824fc5a020341f035e827052dfcc521d
Caleb <56181475+7248510@users.noreply.github.com>
2019-12-21 21:39:48 -0500

    CentOS 7 min
    A script to install essentials for CentOS.

---
+++ cent-min.sh
@@ -0,0 +1,28 @@
+#/usr/bin/sh
+#Parameters
+#cl = Command line #
+#comp = Component #, if you don't want a component just comment out one of the array slots
+install()
+{
+	cl1=('sudo')
+	cl2=('yum')
+	cl3=('install')
+	comp[0]="epel-release"
+	comp[1]="wget"
+	comp[2]="gcc-c++"
+	comp[3]="git"
+	comp[4]="centos-release-scl"
+	comp[5]="rh-python36"
+	echo "$c11 $cl2 $cl3 ${comp[0]}"
+	echo "$c11 $cl2 $cl3 ${comp[1]}"
+	echo "$c11 $cl2 $cl3 ${comp[2]}"
+	echo "$c11 $cl2 $cl3 ${comp[3]}"
+	echo "$c11 $cl2 $cl3 ${comp[4]}"
+	echo "$c11 $cl2 $cl3 ${comp[5]}"
+}
+ena()
+{
+	sudo scl enable rh-python36 bash
+}
+ena
+install
