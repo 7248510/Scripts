8ecc0fed3de4d2ececbfdedd5f2e95cdd6170c3e
Caleb <56181475+7248510@users.noreply.github.com>
2020-04-26 21:46:31 -0400

    Rename

--- java-sdk8.bat
+++ java-sdk8.bat
@@ -3,7 +3,7 @@ echo "Grabbing the Java SDK 8."
 curl https://download.java.net/openjdk/jdk8u40/ri/jdk_ri-8u40-b25-windows-i586-10_feb_2015.zip javasdk8.zip --output javasdk8.zip
 echo "The zip file has been downloaded."
 echo "Please extract the zip file."
-echo "Name the file to 'javasdk8'."
+echo "Name the folder to 'javasdk8'."
 echo "Please press enter once the zip has been extracted."
 TIMEOUT /T 3600 
 echo "Downloading NetBeans SE"
