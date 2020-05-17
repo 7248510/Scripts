ffd277c559ae501752fe9a2445f1c4ef59c73c3d
Caleb <56181475+7248510@users.noreply.github.com>
2020-05-16 23:26:00 -0400

    Update data.sh
    Fixed formatting and added a step from the documentation.

--- data.sh
+++ data.sh
@@ -15,24 +15,25 @@ install()
 	comp[3]="git"
 	comp[4]="centos-release-scl"
 	comp[5]="rh-python36"
-  comp[6]="postgresql12"
-  comp[7]="postgresql12-server"
-  comp[8]="clang"
+  	comp[6]="postgresql12"
+  	comp[7]="postgresql12-server"
+  	comp[8]="clang"
 	$c11 $cl2 $cl3 ${comp[0]}
 	$c11 $cl2 $cl3 ${comp[1]}
 	$c11 $cl2 $cl3 ${comp[2]}
 	$c11 $cl2 $cl3 ${comp[3]}
 	$c11 $cl2 $cl3 ${comp[4]}
 	$c11 $cl2 $cl3 ${comp[5]}
-  $c11 $cl2 $cl3 ${comp[6]}
-  $c11 $cl2 $cl3 ${comp[7]}
-  $c11 $cl2 $cl3 ${comp[8]}
+	$c11 $cl2 $cl3 ${comp[6]}
+	$c11 $cl2 $cl3 ${comp[7]}
+	$c11 $cl2 $cl3 ${comp[8]}
 }
 ena()
 {
 	sudo scl enable rh-python36 bash
-  sudo systemctl enable postgresql-12
-  sudo systemctl start postgresql-12
+	sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
+  	sudo systemctl enable postgresql-12
+  	sudo systemctl start postgresql-12
 }
 update()
 {
