5e58b12708443463a0fe8951ee0eca5d4903e6b0
Caleb <56181475+7248510@users.noreply.github.com>
2020-06-07 20:29:26 -0400

    Update data.sh

--- data.sh
+++ data.sh
@@ -9,9 +9,10 @@ install()
 	cl1=('sudo')
 	cl2=('yum')
 	cl3=('install')
+	c14=('wget')
 	comp[0]="epel-release"
 	comp[1]="wget"
-	comp[2]="https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm"
+	comp[2]="https://dev.mysql.com/get/mysql80-community-release-el7-3.noarch.rpm"
 	comp[3]="git"
 	comp[4]="centos-release-scl"
 	comp[5]="rh-python36"
@@ -20,20 +21,16 @@ install()
   	comp[8]="clang"
 	$c11 $cl2 $cl3 ${comp[0]}
 	$c11 $cl2 $cl3 ${comp[1]}
-	$c11 $cl2 $cl3 ${comp[2]}
+	$c11 $cl4 ${comp[2]}
 	$c11 $cl2 $cl3 ${comp[3]}
 	$c11 $cl2 $cl3 ${comp[4]}
 	$c11 $cl2 $cl3 ${comp[5]}
-	$c11 $cl2 $cl3 ${comp[6]}
-	$c11 $cl2 $cl3 ${comp[7]}
-	$c11 $cl2 $cl3 ${comp[8]}
 }
 ena()
 {
 	sudo scl enable rh-python36 bash
-	sudo /usr/pgsql-12/bin/postgresql-12-setup initdb
-  	sudo systemctl enable postgresql-12
-  	sudo systemctl start postgresql-12
+	#Change the version/uncomment if it's the current version
+	sudo rpm -ivh mysql80-community-release-el7-3.noarch.rpm
 }
 update()
 {
