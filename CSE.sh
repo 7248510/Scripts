aa8d76d215e19a113206d7dfc564001b6276d732
Caleb K <56181475+7248510@users.noreply.github.com>
2020-11-03 23:34:56 -0500

    Update CSE.sh

--- CSE.sh
+++ CSE.sh
@@ -13,6 +13,10 @@ install()
   sudo python3 -m pip install flask-pymongo
   sudo python3 -m pip install flask-bcrypt
   sudo python3 -m pip install bson
+  sudo python3 -m pip install -U Werkzeug==0.16.0
+  sudo python3 -m pip install flask_socketio
+  sudo python3 -m pip install requests
+  sudo firewall-cmd --zone=public --permanent --add-service=http #ufw is not installed on centos minimal. This firewall setting may be different on Ubuntu
 }
 showIP()
 {
@@ -25,9 +29,10 @@ update()
 }
 gitme()
 {
+	cd ../
 	git clone https://github.com/7248510/CSE-Analytics.git
 }
 install
-gitme
+#gitme #Only use gitme if you clone the scripts directory. This is uncommented by default because I'm putting the script in the CSE directory.
 showIP
 #update
