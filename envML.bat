bdb5b8c14f6c4b0e240fa0c977d06a712dc0be21
Caleb K <56181475+7248510@users.noreply.github.com>
2021-02-28 22:36:43 -0500

    Machine learning
    Create a virtual environment. This script is partially done. I have to figure out how to pass/execute the reminder of the script within the newly created virtual environment.

---
+++ envML.bat
@@ -0,0 +1,11 @@
+REM A script for setting up virtual environments(testing this while learning Machine Learning)
+python -m venv env
+cd env/Scripts
+activate
+REM After activating the env the script does not pass the commands. This is a partial script!
+REM cd ../../
+REM py -m pip install --upgrade pip
+REM py -m pip install wheel
+REM py -m pip install -U setuptools
+REM python -m pip install --upgrade requests
+REM python -m pip install --upgrade -r requirements.txt
