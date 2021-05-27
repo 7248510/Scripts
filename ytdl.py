a26213f0016b4bece8b7c0af06f638791b20da51
Caleb K <56181475+7248510@users.noreply.github.com>
2021-05-27 17:53:02 -0400

    Script update
    Spelling, grammar and removing caleb from output. %username% on windows will use the current username.

--- ytdl.py
+++ ytdl.py
@@ -1,9 +1,9 @@
 import subprocess
-def centos():
+def linux():
     #Script has been tested on Centos.
     #This script requires you to add youtube-dl to your path(Windows or Linux) or run the script with the youtube-dl executable(Windows).
     base = 'youtube-dl'
-    yt_url = input("Please input the playlist url you would like to download.\n")
+    yt_url = input("Please input the playlist URL you would like to download.\n")
     flag1 = '--yes-playlist'
     flag2 = '--no-check-certificate'
     flag3 = '--write-sub'
@@ -12,21 +12,21 @@ def centos():
     output_set = "youtube-archive/"
     output_name = input("Please input the folder name you would like to save your files too.\n")
     output_end = "/%(title)s.%(ext)s"
-    print("Your download has started, this is supposed to be for playlists.")
+    print("Your download has started. This is intended for playlists.")
     command = base + space + yt_url + space + flag1 + space + space +flag2 + space + flag3+ space + flag4 +space + "-o " + output_set + output_name + output_end
     subprocess.call(command, shell=False)
 def windows():
     base = 'youtube-dl.exe'
-    yt_url = input("Please input the playlist url you would like to download.\n")
+    yt_url = input("Please input the playlist URL you would like to download.\n")
     flag1 = '--yes-playlist'
     flag2 = '--no-check-certificate'
     flag3 = '--write-sub'
     flag4 = '--playlist-start 1'
     space = " "
-    output_set = "C:\\Users\\Caleb\\Desktop\\"
-    output_name = input("Please input the folder name you would like to save your files too.\n")
+    output_set = "C:\\Users\\%username%\\Desktop\\"
+    output_name = input("Please input the folder name you would like to save your files to.\n")
     output_end = "\\%(title)s.%(ext)s"
-    print("Your download has started, this is supposed to be for playlists.")
+    print("Your download has started. This is intended for playlists.")
     command = base + space + yt_url + space + flag1 + space + space +flag2 + space + flag3+ space + flag4 +space + "-o " + output_set + output_name + output_end
     subprocess.call(command, shell=False)
 def choice():
@@ -35,5 +35,5 @@ def choice():
     if (choiceE == 1):
         windows()
     if (choiceE == 2):
-        centos()
+        linux()
 choice()
